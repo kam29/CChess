44f2cc94b8d277fc7907548a6cd058f173eb4444
kam29 <xmichl02@stud.fit.vutbr.cz>
2015-05-26 22:03:10 +0200

    validace tahu pro krále a jezdce

--- tahy.c
+++ tahy.c
@@ -5,7 +5,19 @@
 
 bool ValidujTah(hra_t* hra, tah_t* tah)
 {
-	
+	if (tah->kdo != hra->pole[tah->zx][tah->zy]) return false;
+	switch (tah->kdo)
+	{
+		case BKRA:
+		case CKRA:
+			if (!((tah->zx == tah->dox && (tah->zy == tah->doy+1 || tah->zy == tah->doy-1)) || (tah->zy == tah->doy && (tah->zx == tah->dox+1 || tah->zx == tah->dox-1)))) return false;
+			break;;
+		case BJEZ:
+		case CJEZ:
+			if (!(((tah->zx == tah->dox+2 || tah->zx == tah->dox-2) && (tah->zy == tah->doy+1 || tah->zy == tah->doy-1)) || ((tah->zy == tah->doy+2 || tah->zy == tah->doy-2) && (tah->zx == tah->dox+1 || tah->zx == tah->dox-1)))) return false;
+			break;
+	}
+	if (!ValidujSach(tah, hra)) return false; // Zkontroluje, jestli se hráč nedostal tahem do šachu
 }
 
 void ProvedTah(hra_t* hra, tah_t* tah);
